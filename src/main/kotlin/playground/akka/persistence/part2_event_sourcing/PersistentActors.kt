f29ba3a948c1a4c05615a595f77b3eccef7c9c71
Milad <miladamery@chmail.ir>
2021-04-01 23:27:40 +0430

    Akka rtjvm persistence 2

--- src/main/kotlin/playground/akka/persistence/part2_event_sourcing/PersistentActors.kt
+++ src/main/kotlin/playground/akka/persistence/part2_event_sourcing/PersistentActors.kt
@@ -13,15 +13,18 @@ class PersistentActors {
     // COMMANDS
     // Which are normal messages that we programmers send to actor
     data class Invoice(val recipient: String, val date: Date, val amount: Int)
+    data class InvoiceBulk(val invoices: List<Invoice>)
+    object ShutDown
 
     // EVENTS
     // The data structure that persistent actors will send to persistence store (journal)
-    data class InvoiceRecorded(val id: Int, val recipient: String, val date: Date, val amount: Int): Serializable
+    data class InvoiceRecorded(val id: Int, val recipient: String, val date: Date, val amount: Int) : Serializable
 
     class Accountant : AbstractPersistentActor() {
         private val log = Logging.getLogger(this)
         private var latestInvoiceId = 0
         private var totalAmount = 0
+
         /**
          * The normal receive method
          */
@@ -49,6 +52,32 @@ class PersistentActors {
                     }
 
                 }
+                .match(InvoiceBulk::class.java) { it ->
+                    /*
+                        1) create Events
+                        2) Persis all the events
+                        3) update the actor state when each event is persisted
+                     */
+                    // 1)
+                    val invoiceIds = latestInvoiceId..(latestInvoiceId + it.invoices.size)
+                    val events = it.invoices.zip(invoiceIds).map { pair ->
+                        val invoice = pair.first
+                        val id = pair.second
+                        InvoiceRecorded(id, invoice.recipient, invoice.date, invoice.amount)
+                    }
+                    // 2)
+                    persistAll(events) { /*3)*/invoiceRecorded ->
+                        latestInvoiceId += 1
+                        totalAmount += invoiceRecorded.amount
+                        log.info("Persisted SINGLE $invoiceRecorded as invoice ${invoiceRecorded.id}, for total amount $totalAmount")
+                    }
+                }
+                .match(ShutDown::class.java) {
+                    // this will be put on the normal mailbox
+                    // that guarantees ShutDown message will be processed after all
+                    // Invoices have been correctly treated.
+                    context.stop(self)
+                }
                 .build()
         }
 
@@ -71,6 +100,26 @@ class PersistentActors {
                 .build()
         }
 
+        /*
+            This method is called if persisting failed.
+            The actor will be STOPPED.
+
+            Best practice: start the actor again after a while
+            (Use Backoff supervisor)
+         */
+        override fun onPersistFailure(cause: Throwable?, event: Any?, seqNr: Long) {
+            log.error("Fail to persist $event because of $cause")
+            super.onPersistFailure(cause, event, seqNr)
+        }
+
+        /*
+            Called if the JOURNAL fails to persist the event
+            The actor is RESUMED.
+         */
+        override fun onPersistRejected(cause: Throwable?, event: Any?, seqNr: Long) {
+            log.error("Persist reject for $event because of the $cause")
+            super.onPersistRejected(cause, event, seqNr)
+        }
     }
 }
 
@@ -87,4 +136,34 @@ fun main() {
             ActorRef.noSender()
         )
     }*/
+
+    /*
+        Persistence Failures
+     */
+
+    /**
+     * Persisting multiple events
+     *
+     * persistAll
+     */
+
+    val newInvoices = (1..5).map {
+        PersistentActors.Invoice("The awesome chair", Date(), it * 2000)
+    }
+    //accountant.tell(PersistentActors.InvoiceBulk(newInvoices), ActorRef.noSender())
+
+    /*
+      NEVER EVER CALL PERSIST OR PERSISTALL FROM FUTURES. otherwise you risk to breaking
+      actor encapsulation because actor thread is free to process messages while you are
+      persisting. and if normal actor thread call persis you suddenly have 2 threads persisting events
+      simultaneously. so because the event order is non-deterministic you risk corrupting actor state
+     */
+
+    /**
+     * Shutdown of persistent actors
+     * Kill or PoisonPill is not ideal/good on persistent actors
+     * because you risk killing the actor before its actually done persisting.
+     *
+     * best practice: Define your own shutdown messages.
+     */
 }
