3d7ec368350b0bda418384bad60b503d001aad25
Milad <miladamery@chmail.ir>
2021-04-09 16:29:36 +0430

    akka rtjvm persistence 11

---
+++ src/main/kotlin/playground/akka/persistence/part3_stores_serialization/Cassandra.kt
@@ -0,0 +1,26 @@
+package playground.akka.persistence.part3_stores_serialization
+
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import akka.actor.Props
+import com.typesafe.config.ConfigFactory
+
+class Cassandra {
+}
+
+fun main() {
+    val system = ActorSystem.create("cassandraStoresSystem", ConfigFactory.load().getConfig("cassandraDemo"))
+    val persistentActor = system.actorOf(Props.create(SimplePersistentActor::class.java), "simpleCassandraPersistentActor")
+
+    /*(1..10).forEach {
+        persistentActor.tell("I Love Akka [$it]", ActorRef.noSender())
+    }
+    persistentActor.tell("print", ActorRef.noSender())
+    persistentActor.tell("snap", ActorRef.noSender())
+
+    (11..20).forEach {
+        persistentActor.tell("I Love Akka [$it]", ActorRef.noSender())
+    }*/
+
+    persistentActor.tell("print", ActorRef.noSender())
+}
